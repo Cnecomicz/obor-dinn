4df3920f2bab562b24181652ba3927fc68c59730
Eric <eric@locallyringed.space>
2026-02-07 16:10:58 -0600

    Red: test_word_creation

---
+++ tests/test_words.py
@@ -0,0 +1,17 @@
+from words import Word
+
+# A Word can be created with required metadata
+def test_word_creation():
+    word = Word(
+        id="testword",
+        roles={"subject", "verb", "object", "other"},
+        ideology={"left-right": 1, "libertarian-authoritarian": 1},
+        rhetoric={"testrhetoric1", "testrhetoric2"},
+        topic={"testtopic1", "testtopic2"}
+    )
+
+    assert word.id == "testword"
+    assert "object" in word.roles
+    assert word.ideology["authority"] == 1
+    assert "testrhetoric2" in word.rhetoric
+    assert "testtopic1" in word.topic
