7f898417b66c0ab12d32c61487f67fc24495a10c
Eric <eric@locallyringed.space>
2026-02-07 16:44:25 -0600

    Refactor: test_word_creation

--- src/backend/words.py
+++ src/backend/words.py
@@ -1,15 +1,28 @@
+from collections import defaultdict
+from enum import Enum
+
+class Ideology(Enum):
+    LEFT = "left"
+    RIGHT = "right"
+    LIBERTARIAN = "libertarian"
+    AUTHORITARIAN = "authoritarian"
+
 class Word:
     def __init__(
         self, 
         id: str, 
         roles: set[str], 
-        ideology: dict[str, int], 
+        ideology: dict[Ideology, int], 
         rhetoric: set[str], 
         topic: set[str]
     ):
         self.id = id
         self.roles = roles
-        self.ideology = ideology
+        self._ideology = defaultdict(int, ideology)
+        self.ideology_vector = (
+            self._ideology[Ideology.RIGHT]-self._ideology[Ideology.LEFT], 
+            self._ideology[Ideology.AUTHORITARIAN]-self._ideology[Ideology.LIBERTARIAN]
+        )
         self.rhetoric = rhetoric
         self.topic = topic
 
--- tests/backend/test_words.py
+++ tests/backend/test_words.py
@@ -1,17 +1,17 @@
-from backend.words import Word
+from backend.words import Ideology, Word
 
 # A Word can be created with required metadata
 def test_word_creation():
     word = Word(
         id="testword",
         roles={"subject", "verb", "object", "other"},
-        ideology={"left-right": 1, "libertarian-authoritarian": 1},
+        ideology={Ideology.LEFT: 1, Ideology.LIBERTARIAN: 1},
         rhetoric={"testrhetoric1", "testrhetoric2"},
         topic={"testtopic1", "testtopic2"}
     )
 
     assert word.id == "testword"
     assert "object" in word.roles
-    assert word.ideology["libertarian-authoritarian"] == 1
+    assert word.ideology_vector == (-1, -1)
     assert "testrhetoric2" in word.rhetoric
     assert "testtopic1" in word.topic
